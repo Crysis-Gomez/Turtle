a22985f8e6bf2ea5b49bbc0f997277fd3f157bcf
Crysis-Gomez <Crysis.gomez@gmail.com>
2014-12-19 21:34:07 +0100

    Added slider and improved painting

--- main.js
+++ main.js
@@ -1,104 +1,166 @@
+var that;
 
 $(document).ready(function(){
     var canvas = document.getElementById('myCanvas');
 	var context= canvas.getContext('2d');
-	var tree = new Tree(context);
-
+	var tree = new Tree(canvas,context);
+	that = tree;
+	
 	var canvas = document.getElementById('myCanvas2');
 	var context= canvas.getContext('2d');
-    var triangle = new Triangle(context);
+    var triangle = new Triangle(canvas,context);
+
+
+    function changeTree(event, ui){
+    	if (event.originalEvent) {
+        	tree.redraw(ui.value);
+        }
+        else {
+            //programmatic change
+            console.log("change22");
+        }
+    }
+    function changeTriangle(event,ui){
+    	if (event.originalEvent) {
+        	triangle.redraw(ui.value);
+        }
+        else {
+            //programmatic change
+            console.log("change22");
+        }
+    }
+
+   $('#tree_slider').slider({
+   	  range: "max",
+      min: 2,
+      max: 10,
+      value: 2,
+	  change: changeTree
+	 
+	});
+
+   $('#triangle_slider').slider({
+   	  range: "max",
+      min: 2,
+      max: 8,
+      value: 2,
+	  change: changeTriangle
+	 
+	});
+
 });
 
 
-var Triangle = function(context){
-
-	var turtle = new Turtle(context);
-	turtle.init(150,500);
-	turtle.setPenColor("#000000");
-	turtle.right(90);
-	drawTriangle(330, 8, 60);
-	
-
-	function drawTriangle(length,depth,angle){
-		drawLine(length, depth,angle);
+var Triangle = function(canvas,context){
+	this.canvas = canvas;
+	this.context = context;
+	this.turtle = new Turtle(context);
+	this.turtle.init(200,500);
+	this.turtle.setPenColor("#000000");
+	this.turtle.right(90);
+		
+	this.drawTriangle = function(length,depth,angle){
+		this.drawLine(length, depth,angle);
 	}
 
-	function drawLine(length,depth,angle){
+	this.drawLine= function(length,depth,angle){
 		if (depth <=0){
-			turtle.forward(length,2,null);
+			this.turtle.forward(length,2,null);
 		}else{
-			drawLine(length / 2, depth - 1, -angle);
-			turtle.right( +angle);
-			drawLine(length / 2, depth - 1, +angle);
-			turtle.right( +angle);
-			drawLine(length / 2, depth - 1, -angle);
-
+			this.drawLine(length / 2, depth - 1, -angle);
+			this.turtle.right( +angle);
+			this.drawLine(length / 2, depth - 1, +angle);
+			this.turtle.right( +angle);
+			this.drawLine(length / 2, depth - 1, -angle);
 		}	
 	}
+
+	this.redraw = function(value){
+
+		this.turtle.init(200,550);
+		
+		if(value % 2==0){
+			this.turtle.right(90);
+		}else{
+			this.turtle.left(-30);
+		}
+
+		this.turtle.setPenColor("#24120a",15);
+		this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+		this.drawTriangle(200, value, 60);
+	}
+
+	this.drawTriangle(200, 2, 60);
 }
 
-var Tree = function(context){
 
-	turtle = new Turtle(context);
-	turtle.init(350,550);
-	turtle.setPenColor("#24120a",15);
+var Tree = function(canvas,context){
+	this.canvas = canvas;
+	this.context = context;
+	this.turtle = new Turtle(context);
+	this.turtle.init(350,550);
+	this.turtle.setPenColor("#24120a",15);
 
-	drawTree(120,10,30,15);
-	drawLeaf();
+	this.redraw = function (value){
 
-	function drawLeaf(){
+		this.turtle.init(350,550);
+		this.turtle.setPenColor("#24120a",15);
+		this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+		this.drawTree(120,value,30,15);
+		this.drawLeaf();
+	}
 
- 		for (var i = 0; i < turtle.savePositions.length; i++) {
- 			 var p = turtle.savePositions[i];
+	this.drawLeaf = function(){
+
+ 		for (var i = 0; i < this.turtle.savePositions.length; i++) {
+ 			 var p = this.turtle.savePositions[i];
  			 var x = p[0];
  			 var y = p[1];
 
- 			context.beginPath();
+ 			this.context.beginPath();
+ 			this.context.strokeStyle ="#0e341a";
+	        this.context.fillStyle = 'green';
 
- 			context.moveTo(x,y);
- 			context.lineWidth = 2;
- 			context.quadraticCurveTo(x+5, y-5, x+10, y);
-			context.quadraticCurveTo(x+5, y+5, x, y);
-			context.moveTo(x,y);
-			context.quadraticCurveTo(x-5, y-5, x-10, y);
-			context.quadraticCurveTo(x-5, y+5, x, y);
+ 		 	this.context.moveTo(x,y);
+ 		 	this.context.lineWidth = 2;
 
-			context.moveTo(x,y);
-			context.quadraticCurveTo(x+5, y-5, x, y-10);
-			context.quadraticCurveTo(x-5, y-5, x, y);
-
-			context.moveTo(x,y);
-			context.quadraticCurveTo(x+5, y+5, x, y+10);
-			context.quadraticCurveTo(x-5, y+5, x, y);
+ 			this.context.quadraticCurveTo(x+5, y-5, x+10, y);
+			this.context.quadraticCurveTo(x+5, y+5, x, y);
+			this.context.moveTo(x,y);
+			this.context.quadraticCurveTo(x-5, y-5, x-10, y);
+			this.context.quadraticCurveTo(x-5, y+5, x, y);
+			this.context.moveTo(x,y);
+			this.context.quadraticCurveTo(x+5, y-5, x, y-10);
+			this.context.quadraticCurveTo(x-5, y-5, x, y);
+			this.context.moveTo(x,y);
+			this.context.quadraticCurveTo(x+5, y+5, x, y+10);
+			this.context.quadraticCurveTo(x-5, y+5, x, y);
 	
-
-		 	context.strokeStyle ="#0e341a";
-	        context.fillStyle = 'green';
-	        context.fill();
-	        context.stroke();
-	        context.closePath();
-	       
+		 	this.context.strokeStyle ="#0e341a";
+	        this.context.fillStyle = 'green';
+	        this.context.fill();
+	        this.context.stroke();
+	        this.context.closePath();
+	        	       
  		};
-
 	}
 
-	function drawTree(length,depth,angle,thinkness){
+	this.drawTree = function(length,depth,angle,thinkness){
 
 		if(depth <=0)return;
-		turtle.forward(length,thinkness,depth);
+		this.turtle.forward(length,thinkness,depth);
 		
-		turtle.right(angle);
-		drawTree(length/1.3,depth-1,angle,thinkness-2);
-		turtle.left(2* angle);
-		drawTree(length/1.3,depth-1,angle,thinkness-2);
-		turtle.right(angle);
-		turtle.forward(-length,thinkness,depth);
+		this.turtle.right(angle);
+		this.drawTree(length/1.3,depth-1,angle,thinkness-2);
+		this.turtle.left(2* angle);
+		this.drawTree(length/1.3,depth-1,angle,thinkness-2);
+		this.turtle.right(angle);
+		this.turtle.forward(-length,thinkness,depth);
 	}
+
+	this.drawTree(120,2,30,15);
+	this.drawLeaf();	
 }
-
-
-
-
 
 var Turtle = function(context){
 	this.context = context;
@@ -108,13 +170,15 @@ var Turtle = function(context){
 	this.sin = 0.0;
 	this.angle=0.0;
 	this.thinkness = 2;
-	this.savePositions = new Array();
+	this.savePositions;
 	
 	this.init = function(x,y){
+	
+		this.savePositions = new Array();
 		this.x = x;
 		this.y = y;
 		this.setAngle(-90);
-		this.context.moveTo(this.x,this.y);
+		this.closeTurtle();
 	}
 
 	this.back = function(x){
@@ -144,7 +208,6 @@ var Turtle = function(context){
 	this.setPenColor= function(color,thinkness){
 		this.strokeStyle = color;
 		this.thinkness = thinkness;
-
 	}	
 
 	this.forward = function(x,thinkness,depth){
